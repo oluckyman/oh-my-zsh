7d9d08a06ff30ae0aa41acafd6470442fb8ac582
oluckyman-mini <oluckyman@example.com>
2013-05-10 18:38:56 +0400

    Pretty git log

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -26,8 +26,9 @@ alias gcount='git shortlog -sn'
 compdef gcount=git
 alias gcp='git cherry-pick'
 compdef _git gcp=git-cherry-pick
-alias glg='git log --stat --max-count=5'
-compdef _git glg=git-log
+#alias glg='git log --stat --max-count=5'
+#compdef _git glg=git-log
+alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias glgg='git log --graph --max-count=5'
 compdef _git glgg=git-log
 alias gss='git status -s'
