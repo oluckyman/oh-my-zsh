073748813029d6b65764cbc72845aacd4fdb9349
oluckyman-pro <oluckyman@example.com>
2013-11-15 12:17:52 +0400

    Refactor git aliases

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,50 +1,33 @@
 # Aliases
+alias gdt="git difftool"
 alias g='git'
-compdef g=git
 alias gst='git status'
-compdef _git gst=git-status
 alias gl='git pull'
-compdef _git gl=git-pull
 alias gup='git fetch && git rebase'
-compdef _git gup=git-fetch
 alias gp='git push'
-compdef _git gp=git-push
-gdv() { git diff -w "$@" | view - }
-compdef _git gdv=git-diff
 alias gc='git commit -v'
-compdef _git gc=git-commit
 alias gca='git commit -v -a'
-compdef _git gca=git-commit
 alias gco='git checkout'
-compdef _git gco=git-checkout
 alias gcm='git checkout master'
 alias gb='git branch'
-compdef _git gb=git-branch
 alias gba='git branch -a'
-compdef _git gba=git-branch
 alias gcount='git shortlog -sn'
-compdef gcount=git
 alias gcp='git cherry-pick'
-compdef _git gcp=git-cherry-pick
-#alias glg='git log --stat --max-count=5'
-#compdef _git glg=git-log
-alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+alias glgg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias glg='glgg --max-count=7'
+alias glaa="glgg --all"
 alias gla="glg --all"
-alias gls="gla --simplify-by-decoration"
-alias glgg='git log --graph --max-count=5'
-compdef _git glgg=git-log
+alias gls="glaa --simplify-by-decoration"
+
 alias gss='git status -s'
-compdef _git gss=git-status
 alias ga='git add'
-compdef _git ga=git-add
 alias gm='git merge'
-compdef _git gm=git-merge
 alias grh='git reset HEAD'
 alias grhh='git reset HEAD --hard'
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
-compdef git-svn-dcommit-push=git
 
 alias gsr='git svn rebase'
 alias gsd='git svn dcommit'
