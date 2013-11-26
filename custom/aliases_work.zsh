2b6a2ec27686409734f3fde78d06f402c2ee38b3
oluckyman-pro <oluckyman@example.com>
2013-11-26 10:53:13 +0400

    Add Auction aliases

--- custom/aliases_work.zsh
+++ custom/aliases_work.zsh
@@ -1,3 +1,8 @@
 # Timeline
 alias cdtl="cd ~/Dropbox/oluckyman-air/Projects/Work/timeline"
 alias rntl="cdtl; http-server -p 8000 &"
+
+# Auction
+alias cdau="cd ~/Projects/Auction/auction"
+alias rnau="cdau; rails server &"
+
