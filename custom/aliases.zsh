7b3e3af736b3d62fe9f021c4bb8ba7132cd1a698
oluckyman-pro <oluckyman@example.com>
2014-06-10 12:11:03 +0400

    Update work aliases. Add grep -r alias

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -3,6 +3,7 @@ alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias du="du -h"
 alias du1="du -d 1"
 alias grep="grep --color=auto"
+alias grepr="grep -r"
 
 # qfind - used to quickly find files that contain a string in a directory
 qfind () {
--- custom/aliases_work.zsh
+++ custom/aliases_work.zsh
@@ -1,6 +1,13 @@
 # Timeline
-alias cdtl="cd ~/Dropbox/oluckyman-air/Projects/Work/timeline"
+alias cdtl="cd ~/Projects/timeline"
 alias rntl="cdtl; http-server -p 8000 &"
+
+# ModulInt
+alias cdmo="cd ~/Projects/modulint; source ~/virtualenv_home/develop/bin/activate"
+alias rnmo="cdmo; python web.py _rnmo_"
+alias rnmok="kill \$(ps | grep '[p]ython web.py _rnmo_' | awk '{print \$1}'); rnmo"
+alias rnmol="cdmo; mongod &; python web.py _rnmo_"
+alias rnmolk="kill \$(ps | grep '[p]ython web.py _rnmo_' | awk '{print \$1}'); rnmol"
 
 # Auction
 alias cdau="cd ~/Projects/Auction/auction"
